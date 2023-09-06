284676505b01fbd4967aff5629151a9988a3bd57
Emma Walsh <walshemma33@outlook.com>
2023-09-06 20:09:31 +0100

    Add mealItem component and render into avail meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,7 +1,8 @@
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
+  const DUMMY_MEALS = [
     {
       id: 'm1',
       name: 'Sushi',
@@ -29,7 +30,14 @@ const DUMMY_MEALS = [
   ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => (
+      <MealItem 
+        key={meal.id} 
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+        />
+      ));
 
     return <section className={classes.meals}>
       <Card>
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
+
+    const price = `£${props.price.toFixed(2)}`
+
+    return <li key={props.id} className={classes.meal}>
+        <div>
+            <h3>{props.name}</h3>
+            <p className={classes.description}>{props.description}</p>
+            <p className={classes.price}>{price}</p>
+        </div>
+        <div>
+
+        </div>
+    </li>
+}
+
+export default MealItem;
