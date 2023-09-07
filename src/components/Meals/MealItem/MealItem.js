adce6f3cd8cd1c512dfa7713eeeb2800844a8f6a
Emma Walsh <walshemma33@outlook.com>
2023-09-07 19:35:44 +0100

    Fix duplicate key issue on input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -13,7 +13,7 @@ const MealItem = (props) => {
             <p className={classes.price}>{price}</p>
         </div>
         <div>
-            <MealItemForm/>
+            <MealItemForm id={props.id}/>
         </div>
     </li>
 }
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,10 +2,10 @@ import React from "react";
 import classes from './MealItemForm.module.css';
 import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
     return <div className={classes.form}>
         <Input label="Amount" className={classes.input} input={{
-            id: "amount",
+            id: "amount" + props.id,
             type: "text",
             step: "2",
             min: "1",
